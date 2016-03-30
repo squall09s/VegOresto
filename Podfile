f591bb7fd45f77cb78d3481ab1806183273d82c5
Squall09s <squall.leonhart@me.com>
2016-03-30 15:20:14 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'VegoResto' do
+
+pod 'FBAnnotationClusteringSwift'
+pod 'Pushwoosh', '~> 3.1.2'
+
+
+end
+
---
+++ VegoResto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VegoResto
+//
+//  Created by Laurent Nicolas on 30/03/2016.
+//  Copyright © 2016 Nicolas Laurent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
