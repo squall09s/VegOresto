962c51c1bbff5e1c42519fc1b35ef0f4815c7c09
Micha Mazaheri <micha@mazaheri.me>
2017-11-19 23:35:20 +0100

    Refactor Restaurant mapping code

--- VegOresto/NSManagedObject/Restaurant.swift
+++ VegOresto/NSManagedObject/Restaurant.swift
@@ -82,172 +82,6 @@ class Restaurant: NSManagedObject, Mappable {
         return CategorieRestaurant.VeganFriendly
     }
 
-    func updateDataMontantMoyen() {
-
-        let dico : [ String : String ] = ["montant_8": "inférieur à 8€",
-                                          "montant_15": "inférieur à 15€",
-                                          "montant_1530": "15-30€",
-                                          "montant_3060": "30-60€",
-                                          "montant_60": "plus de 60€"]
-
-        if let _key = self.montant_moyen {
-
-            if let val = dico[_key] {
-
-                self.montant_moyen = val
-
-            } else {
-
-                Debug.log(object: "ERROR - [updateDataMontantMoyen] key \(_key) not found")
-            }
-
-        }
-    }
-
-    func updateDataTypeEtablissement() {
-
-        let dico : [ String : String ] = ["vpc": "Restauration rapide, à emporter ou à domicile",
-                                          "chambre": "Chambre d'hôtes",
-                                          "resto": "Restaurant",
-                                          "hotel": "Hôtel-restaurant"]
-
-        if let _key = self.type_etablissement {
-
-            if let val = dico[_key] {
-
-                self.type_etablissement = val
-
-            } else {
-
-                Debug.log(object: "ERROR - [updateDataTypeEtablissement] key \(_key) not found")
-            }
-
-        }
-    }
-
-    func updateDataTypeAmbiance(ambiances: [String]) {
-
-        let dico : [ String : String ] = ["branche": "Branché",
-                                          "bistro": "Bistrot de caractère",
-                                          "cosy": "Cosy",
-                                          "spectacle": "Spectacle",
-                                          "patrimoine": "Patrimoine",
-                                          "vintage": "Vintage",
-                                          "jardin": "Jardin",
-                                          "hote": "Table d’hôte",
-                                          "romantique": "Romantique",
-                                          "dansant": "Dansant",
-                                          "eau": "Au bord de l'eau"]
-        var i = 0
-
-        var resultat = ""
-
-        for _key in ambiances {
-
-            if let val = dico[_key] {
-
-                if i > 0 {
-
-                    resultat += ", "
-                }
-
-                resultat += val
-
-                i += 1
-
-            } else {
-
-                Debug.log(object: "ERROR - [updateDataTypeAmbiance] key \(_key) not found")
-            }
-
-        }
-
-        self.ambiance = resultat
-    }
-
-    func updateDataInfluenceGastro(influances: [String]) {
-
-        let dico : [ String : String ] = ["francais_gastro": "Gastronomique français",
-                                          "francais_region": "Régional français",
-                                          "indien": "Indien",
-                                          "italien": "Italien",
-                                          "libanais": "Libanais",
-                                          "marocain": "Marocain",
-                                          "mexicain": "Mexicain",
-                                          "turc": "Turc",
-                                          "vietnamien": "Vietnamien",
-                                          "thai": "Thailandais",
-                                          "japonais": "Japonais",
-                                          "autre": "Autre",
-                                          "espagnol": "Espagnol",
-                                          "tunisien": "Tunisien",
-                                          "chinois": "Chinois",
-                                          "coreen": "Coréen",
-                                          "creole": "Créole"]
-
-        var i = 0
-
-        var resultat = ""
-
-                for _key in influances {
-
-                    if let val = dico[_key] {
-
-                        if i > 0 {
-
-                            resultat += ", "
-                        }
-
-                        resultat += val
-
-                        i += 1
-
-                    } else {
-
-                        Debug.log(object: "ERROR - [updateDataInfluenceGastro] key \(_key) not found")
-                    }
-                }
-
-        self.influence_gastronomique = resultat
-
-    }
-
-    func updateDataMoyensDePaiement(moyensPaiement: [String]) {
-
-        let dico : [ String : String ] = ["cb": "Carte Bleue",
-                                          "cheque": "Chèque",
-                                          "espece": "Espèces",
-                                          "ticket": "Ticket resto",
-                                          "ae": "American Express",
-                                          "vacances": "Chèque vacances"]
-
-        var i = 0
-
-        var resultat = ""
-
-                for _key in moyensPaiement {
-
-                    if let val = dico[_key] {
-
-                        if i > 0 {
-
-                            resultat += ", "
-                        }
-
-                        resultat += val
-
-                        i += 1
-
-                    } else {
-
-                        Debug.log(object: "ERROR - [updateDataMoyensDePaiement] key \(_key) not found")
-                    }
-
-        }
-
-        self.moyens_de_paiement = resultat
-    }
-
     var coordinate: CLLocationCoordinate2D? {
         if let latitude = lat?.doubleValue, let longitude = lon?.doubleValue {
             return CLLocationCoordinate2DMake(latitude, longitude)
@@ -309,109 +143,84 @@ class Restaurant: NSManagedObject, Mappable {
     }
     
     func mapping(map: Map) {
-        
-        identifier <-  map["id"]
-        name <-  map["title"]
-        ville <-  map["ville"]
-        
-        if let _img: [String] = map.JSON["thumbnails"] as? [String] {
-            
-            var image_str_array = ""
-            
-            var _index = 0
-            for _imgTmp: String in _img {
-                
-                if _index == 0 {
-                    image_str_array = _imgTmp
-                } else {
-                    image_str_array += ( "|" + _imgTmp)
-                }
-                
-                _index += 1
-            }
-            
-            self.image = image_str_array
-        }
-        
-        phone <- map["tel_public"]
-        montant_moyen <- map["prix"]
-        
+        self.identifier <-  map["id"]
+        self.name <-  map["title"]
+        self.ville <-  map["ville"]
+        self.phone <- map["tel_public"]
+        self.montant_moyen <- map["prix"]
         self.facebook <- map["fb"]
         self.website <- map["site"]
         self.mail <- map["email"]
-        
-        self.address <- map["adresse"]
-        self.address = (self.address ?? "").removingHTMLTags()
-        
-        self.resume <- map["accroche"]
-        self.resume = (self.resume ?? "").removingHTMLTags()
-        
-        self.montant_moyen <- map["prix"]
-        
-        self.updateDataMontantMoyen()
-        
         self.absolute_url <- map["permalink"]
-        self.type_etablissement <- map["type"]
-        self.updateDataTypeEtablissement()
         
-        if let _cat = map.JSON["cat"] as? [String] {
-            for categoryName in _cat {
+        self.animaux_bienvenus = NSNumber(value: ((map.JSON["anim"] as? String) == "oui"))
+        self.isVegan = NSNumber(value: (map.JSON["vegan"] as? Int == 1))
+        
+        if let address = map.JSON["adresse"] as? String {
+            self.address = address.removingHTMLTags()
+        }
+        
+        if let resume = map.JSON["accroche"] as? String {
+            self.resume = resume.removingHTMLTags()
+        }
+        
+        if let montant_moyen = map.JSON["prix"] as? String {
+            self.montant_moyen = Restaurant.mapMontantMoyen(montant_moyen)
+        }
+        
+        if let type_etablissement = map.JSON["type"] as? String {
+            self.type_etablissement = Restaurant.mapTypeEtablissement(type_etablissement)
+        }
+        
+        if let gastr = map.JSON["gastr"] as? [String] {
+            self.influence_gastronomique = Restaurant.mapInfluenceGastro(gastr)
+        }
+        
+        if let moyensPaiement = map.JSON["payment"] as? [String] {
+            self.moyens_de_paiement = Restaurant.mapMoyensDePaiement(moyensPaiement)
+        }
+        
+        if let ambiances = map.JSON["ambiance"] as? [String] {
+            self.ambiance = Restaurant.mapAmbiance(ambiances)
+        }
+        
+        if let votes = map.JSON["votes"] as? [String:Any], let avg = votes["avg"] as? Double {
+            self.rating = NSNumber(value: avg)
+        }
+        
+        if let imageURLStrings = map.JSON["thumbnails"] as? [String] {
+            self.image = imageURLStrings.joined(separator: "|")
+        }
+        
+        if let lon = map.JSON["lon"] as? Double, let lat = map.JSON["lat"] as? Double {
+            self.lat = NSNumber(value: lat)
+            self.lon = NSNumber(value: lon)
+            self.radius = NSNumber(value: (map.JSON["rad"] as? Double) ?? 0)
+        }
+        
+        if let categories = map.JSON["cat"] as? [String] {
+            for categoryName in categories {
                 addCategorieCulinaire(name: Restaurant.mapCategorieCulinaireName(categoryName))
             }
-        }
-        
-        if let _gast = map.JSON["gastr"] as? [String] {
-            
-            self.updateDataInfluenceGastro(influances: _gast)
-        }
-        
-        if let _moyensPaiement = map.JSON["payment"] as? [String] {
-            
-            self.updateDataMoyensDePaiement(moyensPaiement: _moyensPaiement)
-        }
-        
-        if let _ambiances = map.JSON["ambiance"] as? [String] {
-            self.updateDataTypeAmbiance(ambiances: _ambiances)
-        }
-        
-        if let _votes = map.JSON["votes"] as? [String : Any] {
-            
-            if let _avg = _votes["avg"] as? Double {
-                self.rating = NSNumber(value: _avg)
-            }
-        }
-        
-        if let _lon = map.JSON["lon"] as? Double,
-            let _lat = map.JSON["lat"] as? Double {
-            self.lat = NSNumber(value: _lat)
-            self.lon = NSNumber(value: _lon)
-            if let _radius = map.JSON["rad"] as? Double {
-                self.radius = NSNumber(value: _radius)
-            } else {
-                self.radius = NSNumber(value: 0)
-            }
-        }
-        
-        if let _anim = map.JSON["anim"] as? String {
-            
-            self.animaux_bienvenus = NSNumber(value: (_anim == "oui") )
-            
-        } else {
-            
-            self.animaux_bienvenus = NSNumber(value: false )
-        }
-        
-        if let _veg = map.JSON["vegan"] as? Int {
-            
-            self.isVegan = NSNumber(value: (_veg == 1) )
-            
-        } else {
-            
-            self.isVegan = NSNumber(value: false )
         }
     }
     
     // MARK: Mapping Helpers
+    
+    static func mapMontantMoyen(_ key: String) -> String {
+        let d = [
+            "montant_8": "inférieur à 8€",
+            "montant_15": "inférieur à 15€",
+            "montant_1530": "15-30€",
+            "montant_3060": "30-60€",
+            "montant_60": "plus de 60€"
+        ]
+        guard let val = d[key] else {
+            Debug.log(object: "ERROR - [mapMontantMoyen] key \(key) not found")
+            return key
+        }
+        return val
+    }
     
     static func mapCategorieCulinaireName(_ cat: String) -> String {
         let d = [
@@ -444,5 +253,89 @@ class Restaurant: NSManagedObject, Mappable {
             return cat
         }
         return val
+    }
+    
+    static func mapTypeEtablissement(_ typeEtablissement: String) -> String {
+        let d = [
+            "vpc": "Restauration rapide, à emporter ou à domicile",
+            "chambre": "Chambre d'hôtes",
+            "resto": "Restaurant",
+            "hotel": "Hôtel-restaurant"
+        ]
+        guard let val = d[typeEtablissement] else {
+            Debug.log(object: "ERROR - [mapTypeEtablissement] key \(typeEtablissement) not found")
+            return typeEtablissement
+        }
+        return val
+    }
+    
+    static func mapInfluenceGastro(_ influences: [String]) -> String {
+        let d = [
+            "francais_gastro": "Gastronomique français",
+            "francais_region": "Régional français",
+            "indien": "Indien",
+            "italien": "Italien",
+            "libanais": "Libanais",
+            "marocain": "Marocain",
+            "mexicain": "Mexicain",
+            "turc": "Turc",
+            "vietnamien": "Vietnamien",
+            "thai": "Thailandais",
+            "japonais": "Japonais",
+            "autre": "Autre",
+            "espagnol": "Espagnol",
+            "tunisien": "Tunisien",
+            "chinois": "Chinois",
+            "coreen": "Coréen",
+            "creole": "Créole"
+        ]
+        return influences.map({ key -> String in
+            guard let val = d[key] else {
+                Debug.log(object: "ERROR - [mapInfluenceGastro] key \(key) not found")
+                return key
+            }
+            return val
+        }).joined(separator: ", ")
+    }
+    
+    static func mapMoyensDePaiement(_ moyensPaiement: [String]) -> String {
+        let d = [
+            "cb": "Carte Bleue",
+            "cheque": "Chèque",
+            "espece": "Espèces",
+            "ticket": "Ticket resto",
+            "ae": "American Express",
+            "vacances": "Chèque vacances"
+        ]
+        return moyensPaiement.map({ key -> String in
+            guard let val = d[key] else {
+                Debug.log(object: "ERROR - [mapMoyensDePaiement] key \(key) not found")
+                return key
+            }
+            return val
+        }).joined(separator: ", ")
+    }
+    
+    static func mapAmbiance(_ ambiances: [String]) -> String {
+        let d = [
+            "branche": "Branché",
+            "bistro": "Bistrot de caractère",
+            "cosy": "Cosy",
+            "spectacle": "Spectacle",
+            "patrimoine": "Patrimoine",
+            "vintage": "Vintage",
+            "jardin": "Jardin",
+            "hote": "Table d’hôte",
+            "romantique": "Romantique",
+            "dansant": "Dansant",
+            "eau": "Au bord de l'eau"
+        ]
+        return ambiances.map({ key -> String in
+            guard let val = d[key] else {
+                Debug.log(object: "ERROR - [mapAmbiance] key \(key) not found")
+                return key
+            }
+            return val
+        }).joined(separator: ", ")
     }
 }
